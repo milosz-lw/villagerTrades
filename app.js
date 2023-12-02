8a575b4fa541e0aea8bd481c546e61c605d24945
Miłosz Ławacz <miloszl2000@outlook.com>
2023-12-02 22:52:56 +0100

    added a select with villagers' professions using v-for added a function that handles a change of profession

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
+const professionInput = document.getElementById('profession')
+
 const app = Vue.createApp({
     data(){
         return{
-            
+            currentProfession: 'a',
+            professions:[
+                {name: 'armorer'},
+                {name: 'butcher'},
+                {name: 'cartographer'},
+                {name: 'cleric'},
+                {name: 'farmer'},
+                {name: 'fisherman'},
+                {name: 'fletcher'},
+                {name: 'leatherworker'},
+                {name: 'librarian'},
+                {name: 'mason'},
+                {name: 'shepherd'},
+                {name: 'toolsmith'},
+                {name: 'weaponsmith'}
+            ]
         }
     },
     methods:{
-
+        changeProfession(e){
+            this.currentProfession = e.target.options[e.target.options.selectedIndex].text
+        }
     }
 })
 
