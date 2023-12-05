3461f9c3db6eab05c8b963737989baa131759365
Miłosz Ławacz <80455099+Kartow@users.noreply.github.com>
2023-12-05 10:15:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const app = Vue.createApp({
     data(){
         return{
             currentProfession: 'Armorer',
+            currentProfessionIndex: 0,
             professions:[
                 {name: 'Armorer', products:[
                     {name: 'Emerald', wantedItem: 'Coal', price: 15},
-                    {name: 'Iron Leggins', wantedItem: 'Emerald', price: 7},
+                    {name: 'Iron Leggings', wantedItem: 'Emerald', price: 7},
                     {name: 'Iron Boots', wantedItem: 'Emerald', price: 4},
                     {name: 'Iron Helmet', wantedItem: 'Emerald', price: 5},
                     {name: 'Iron Chestplate', wantedItem: 'Emerald', price: 9},
@@ -22,7 +23,19 @@ const app = Vue.createApp({
                     {name: 'Enchanted Diamond Helmet', wantedItem: 'Emerald', minPrice: 13, maxPrice: 27},
                     {name: 'Enchanted Diamond Chestplate', wantedItem: 'Emerald', minPrice: 21, maxPrice: 35}
                 ]},
-                {name: 'Butcher'},
+                {name: 'Butcher', products:[
+                    {name: 'Emerald', wantedItem: 'Raw Chicken', price: 14},
+                    {name: 'Emerald', wantedItem: 'Raw Porkchop', price: 7},
+                    {name: 'Emerald', wantedItem: 'Raw Rabbit', price: 4},
+                    {name: 'Rabbit Stew', wantedItem: 'Emerald', price: 1},
+                    {name: 'Emerald', wantedItem: 'Coal', price: 15},
+                    {name: 'Cooked Porkchop', wantedItem: 'Emerald', price: 1},
+                    {name: 'Cooked Chicken', wantedItem: 'Emerald', price: 1},
+                    {name: 'Emerald', wantedItem: 'Raw Mutton', price: 7},
+                    {name: 'Emerald', wantedItem: 'Raw Beef', price: 10},
+                    {name: 'Emerald', wantedItem: 'Dried Kelp Block', price: 10},
+                    {name: 'Emerald', wantedItem: 'Sweet Berries', price: 10}
+                ]},
                 {name: 'Cartographer'},
                 {name: 'Cleric'},
                 {name: 'Farmer'},
@@ -40,6 +53,7 @@ const app = Vue.createApp({
     methods:{
         changeProfession(e){
             this.currentProfession = e.target.options[e.target.options.selectedIndex].text
+            this.currentProfessionIndex = e.target.options.selectedIndex
         }
     }
 })
