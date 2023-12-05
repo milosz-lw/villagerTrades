ef7142905bd03d18836e94706e27e2cd4c9317b7
Miłosz Ławacz <80455099+Kartow@users.noreply.github.com>
2023-12-05 12:14:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,36 +5,72 @@ const app = Vue.createApp({
             currentProfessionIndex: 0,
             professions:[
                 {name: 'Armorer', products:[
-                    {name: 'Emerald', wantedItem: 'Coal', price: 15},
-                    {name: 'Iron Leggings', wantedItem: 'Emerald', price: 7},
-                    {name: 'Iron Boots', wantedItem: 'Emerald', price: 4},
-                    {name: 'Iron Helmet', wantedItem: 'Emerald', price: 5},
-                    {name: 'Iron Chestplate', wantedItem: 'Emerald', price: 9},
-                    {name: 'Emerald', wantedItem: 'Iron Ignot', price: 4},
-                    {name: 'Bell', wantedItem: 'Emerald', price: 36},
-                    {name: 'Chainmail Boots', wantedItem: 'Emerald', price: 1},
-                    {name: 'Chainmail Leggings', wantedItem: 'Emerald', price: 3},
-                    {name: 'Emerald', wantedItem: 'Lava Bucket', price: 1},
-                    {name: 'Emerald', wantedItem: 'Diamond', price: 1},
-                    {name: 'Chainmail Helmet', wantedItem: 'Emerald', price: 1},
-                    {name: 'Chainmail Chestplate', wantedItem: 'Emerald', price: 4},
-                    {name: 'Enchanted Diamond Leggings', wantedItem: 'Emerald', minPrice: 19, maxPrice: 33},
-                    {name: 'Enchanted Diamond Boots', wantedItem: 'Emerald', minPrice: 13, maxPrice: 27},
-                    {name: 'Enchanted Diamond Helmet', wantedItem: 'Emerald', minPrice: 13, maxPrice: 27},
-                    {name: 'Enchanted Diamond Chestplate', wantedItem: 'Emerald', minPrice: 21, maxPrice: 35}
+                    {name: 'Emerald', selected: true, wantedItems:[
+                        {name: 'Coal', price: 15},
+                        {name: 'Iron Ingot', price: 4},
+                        {name: 'Lava Bucket', price: 1},
+                        {name: 'Diamond', price: 1},
+                    ]},
+                    {name: 'Iron Leggings', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 7}
+                    ]},
+                    {name: 'Iron Boots', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 7}
+                    ]},
+                    {name: 'Iron Helmet', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 5}
+                    ]},
+                    {name: 'Iron Chestplate', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 9}
+                    ]},
+                    {name: 'Bell', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 36}
+                    ]},
+                    {name: 'Chainmail Boots', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]},
+                    {name: 'Chainmail Leggings', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 3}
+                    ]},
+                    {name: 'Chainmail Helmet', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]},
+                    {name: 'Chainmail Chestplate', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 4}
+                    ]},
+                    {name: 'Enchanted Diamond Leggings', selected: false, wantedItems:[
+                        {name: 'Emerald', minPrice: 19, maxPrice: 33}
+                    ]},
+                    {name: 'Enchanted Diamond Boots', selected: false, wantedItems:[
+                        {name: 'Emerald', minPrice: 13, maxPrice: 27}
+                    ]},
+                    {name: 'Enchanted Diamond Helmet', selected: false, wantedItems:[
+                        {name: 'Emerald', minPrice: 13, maxPrice: 27}
+                    ]},
+                    {name: 'Enchanted Diamond Chestplate', selected: false, wantedItems:[
+                        {name: 'Emerald', minPrice: 21, maxPrice: 35}
+                    ]}
                 ]},
                 {name: 'Butcher', products:[
-                    {name: 'Emerald', wantedItem: 'Raw Chicken', price: 14},
-                    {name: 'Emerald', wantedItem: 'Raw Porkchop', price: 7},
-                    {name: 'Emerald', wantedItem: 'Raw Rabbit', price: 4},
-                    {name: 'Rabbit Stew', wantedItem: 'Emerald', price: 1},
-                    {name: 'Emerald', wantedItem: 'Coal', price: 15},
-                    {name: 'Cooked Porkchop', wantedItem: 'Emerald', price: 1},
-                    {name: 'Cooked Chicken', wantedItem: 'Emerald', price: 1},
-                    {name: 'Emerald', wantedItem: 'Raw Mutton', price: 7},
-                    {name: 'Emerald', wantedItem: 'Raw Beef', price: 10},
-                    {name: 'Emerald', wantedItem: 'Dried Kelp Block', price: 10},
-                    {name: 'Emerald', wantedItem: 'Sweet Berries', price: 10}
+                    {name: 'Emerald', selected: false, wantedItems:[
+                        {name: 'Raw Chicken', price: 14},
+                        {name: 'Raw Porkchop', price: 7},
+                        {name: 'Raw Rabbit', price: 4},
+                        {name: 'Coal', price: 15},
+                        {name: 'Raw Mutton', price: 7},
+                        {name: 'Raw Beef', price: 10},
+                        {name: 'Dried Kelp Block', price: 10},
+                        {name: 'Sweet Berries', price: 10},
+                    ]},
+                    {name: 'Rabbit Stew', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]},
+                    {name: 'Cooked Porkchop', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]},
+                    {name: 'Cooked Chicken', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]}
                 ]},
                 {name: 'Cartographer'},
                 {name: 'Cleric'},
