fa8e17ae2ad2807f9129b1154d08a7b1cf9d8566
Miłosz Ławacz <miloszl2000@outlook.com>
2023-12-07 22:24:54 +0100

    - after choosing what to buy, wanted items with prices appear on left - the app is now responsive

--- app.js
+++ app.js
@@ -2,27 +2,77 @@ const app = Vue.createApp({
     data(){
         return{
             currentProfession: 'Armorer',
+            currentProfessionIndex: 0,
+            currentProductIndex: 2,
             professions:[
                 {name: 'Armorer', products:[
-                    {name: 'Emerald', wantedItem: 'Coal', price: 15},
-                    {name: 'Iron Leggins', wantedItem: 'Emerald', price: 7},
-                    {name: 'Iron Boots', wantedItem: 'Emerald', price: 4},
-                    {name: 'Iron Helmet', wantedItem: 'Emerald', price: 5},
-                    {name: 'Iron Chestplate', wantedItem: 'Emerald', price: 9},
-                    {name: 'Emerald', wantedItem: 'Iron Ignot', price: 4},
-                    {name: 'Bell', wantedItem: 'Emerald', price: 36},
-                    {name: 'Chainmail Boots', wantedItem: 'Emerald', price: 1},
-                    {name: 'Chainmail Leggings', wantedItem: 'Emerald', price: 3},
-                    {name: 'Emerald', wantedItem: 'Lava Bucket', price: 1},
-                    {name: 'Emerald', wantedItem: 'Diamond', price: 1},
-                    {name: 'Chainmail Helmet', wantedItem: 'Emerald', price: 1},
-                    {name: 'Chainmail Chestplate', wantedItem: 'Emerald', price: 4},
-                    {name: 'Enchanted Diamond Leggings', wantedItem: 'Emerald', minPrice: 19, maxPrice: 33},
-                    {name: 'Enchanted Diamond Boots', wantedItem: 'Emerald', minPrice: 13, maxPrice: 27},
-                    {name: 'Enchanted Diamond Helmet', wantedItem: 'Emerald', minPrice: 13, maxPrice: 27},
-                    {name: 'Enchanted Diamond Chestplate', wantedItem: 'Emerald', minPrice: 21, maxPrice: 35}
+                    {name: 'Emerald', selected: true, wantedItems:[
+                        {name: 'Coal', price: 15},
+                        {name: 'Iron Ingot', price: 4},
+                        {name: 'Lava Bucket', price: 1},
+                        {name: 'Diamond', price: 1},
+                    ]},
+                    {name: 'Iron Leggings', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 7}
+                    ]},
+                    {name: 'Iron Boots', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 7}
+                    ]},
+                    {name: 'Iron Helmet', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 5}
+                    ]},
+                    {name: 'Iron Chestplate', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 9}
+                    ]},
+                    {name: 'Bell', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 36}
+                    ]},
+                    {name: 'Chainmail Boots', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]},
+                    {name: 'Chainmail Leggings', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 3}
+                    ]},
+                    {name: 'Chainmail Helmet', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]},
+                    {name: 'Chainmail Chestplate', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 4}
+                    ]},
+                    {name: 'Enchanted Diamond Leggings', selected: false, wantedItems:[
+                        {name: 'Emerald', minPrice: 19, maxPrice: 33}
+                    ]},
+                    {name: 'Enchanted Diamond Boots', selected: false, wantedItems:[
+                        {name: 'Emerald', minPrice: 13, maxPrice: 27}
+                    ]},
+                    {name: 'Enchanted Diamond Helmet', selected: false, wantedItems:[
+                        {name: 'Emerald', minPrice: 13, maxPrice: 27}
+                    ]},
+                    {name: 'Enchanted Diamond Chestplate', selected: false, wantedItems:[
+                        {name: 'Emerald', minPrice: 21, maxPrice: 35}
+                    ]}
                 ]},
-                {name: 'Butcher'},
+                {name: 'Butcher', products:[
+                    {name: 'Emerald', selected: false, wantedItems:[
+                        {name: 'Raw Chicken', price: 14},
+                        {name: 'Raw Porkchop', price: 7},
+                        {name: 'Raw Rabbit', price: 4},
+                        {name: 'Coal', price: 15},
+                        {name: 'Raw Mutton', price: 7},
+                        {name: 'Raw Beef', price: 10},
+                        {name: 'Dried Kelp Block', price: 10},
+                        {name: 'Sweet Berries', price: 10},
+                    ]},
+                    {name: 'Rabbit Stew', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]},
+                    {name: 'Cooked Porkchop', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]},
+                    {name: 'Cooked Chicken', selected: false, wantedItems:[
+                        {name: 'Emerald', price: 1}
+                    ]}
+                ]},
                 {name: 'Cartographer'},
                 {name: 'Cleric'},
                 {name: 'Farmer'},
@@ -40,6 +90,10 @@ const app = Vue.createApp({
     methods:{
         changeProfession(e){
             this.currentProfession = e.target.options[e.target.options.selectedIndex].text
+            this.currentProfessionIndex = e.target.options.selectedIndex
+        },
+        changeProduct(index){
+            this.currentProductIndex = index;
         }
     }
 })
